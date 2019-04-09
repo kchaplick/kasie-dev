89b412ef7697ded2f16487bb2103d318a2c838d6
Kasie <kchaplick92@gmail.com>
2019-04-08 23:33:42 -0400

    portfolio images

--- assets/javascript/web.js
+++ assets/javascript/web.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+  let height= $(window).height()
+  console.log(height);
+  $("section").css('min-height', height);
+
   $('.sidenav').sidenav();
+
 });
 
+$(window).resize(function(){
+let height= $(window).height()
+  console.log(height);
+  $("section").css('min-height', height);
+});
