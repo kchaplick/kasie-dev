d4c5487ce583b16c0386bfcd0554813e7cbfd43d
Kasie <kchaplick92@gmail.com>
2019-04-10 00:00:38 -0400

    skills timing fix

--- assets/javascript/web.js
+++ assets/javascript/web.js
@@ -24,7 +24,7 @@ $(window).on('scroll', function(){
   	$('.skillbar').each(function(){
       $(this).find('.skillbar-bar').animate({
         width:$(this).attr('data-percent')
-      },6000);
+      },3000);
     });
   }
 });
