592d0c6261dbc2e7c72af87e7ecd8719775a66c2
Kasie <kchaplick92@gmail.com>
2019-04-09 23:40:42 -0400

    about me

--- assets/javascript/web.js
+++ assets/javascript/web.js
@@ -12,3 +12,20 @@ let height= $(window).height()
   console.log(height);
   $("section").css('min-height', height);
 });
+
+
+var skillsDiv = $('#skills');
+
+$(window).on('scroll', function(){
+	var winT = $(window).scrollTop(),
+  	winH = $(window).height(),
+  	skillsT = skillsDiv.offset().top;
+  if(winT + winH  > skillsT){
+  	$('.skillbar').each(function(){
+      $(this).find('.skillbar-bar').animate({
+        width:$(this).attr('data-percent')
+      },6000);
+    });
+  }
+});
+
